4804f0fa5a3e4f737b7893c1afb8ce22ac52eb4a
Adi-The-Pro <adira42004@gmail.com>
2025-11-26 14:12:30 +0530

    User Entities And Services Created

---
+++ lib/src/main/java/ticket/booking/entities/User.java
@@ -0,0 +1,18 @@
+package ticket.booking.entities;
+
+import java.util.List;
+
+public class User {
+
+    private String name;
+    private String password;
+    private String hashedPassword;
+    private List<Ticket> tickets;
+    private String userId;
+
+
+
+    public static void main(String[] args){
+
+    }
+}
